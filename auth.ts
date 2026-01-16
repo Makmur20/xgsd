afab9aae4b20bc505afa85390ca3802d4c960e43
Makmur20 <makmurmulyono@gmail.com>
2026-01-16 08:00:28 +0700

    auth-update

--- auth.ts
+++ auth.ts
@@ -1,24 +1,21 @@
 import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import {prisma} from "@/lib/prisma";
 import Google from "next-auth/providers/google";
- 
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
-  session: {strategy:"jwt"},
-  pages:{
-    signIn: "/signin",
-  },
+  session: { strategy: "jwt" },
   callbacks: {
     jwt({ token, user }) {
       if (user) token.role = user.role;
       return token;
     },
-    session( { session, token }) {
-      session.user.id = token.sub;
-      session.user.role = token.role;
+    session({ session, token }) {
+      session.user.id = token.sub!;
+      session.user.role = token.role as string;
       return session;
-    }
-  }
-})
+    },
+  },
+});
--- types/authjs.d.ts
+++ types/authjs.d.ts
@@ -1,19 +1,20 @@
-import { type DefaultSession } from "next-auth";
-import { JWT } from "next-auth/jwt";
+import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
-    interface Session {
-        user: User & DefaultSession["user"];
-    }
+  interface Session {
+    user: {
+      id: string;
+      role: string;
+    } & DefaultSession["user"];
+  }
 
-    interface User {
-        role: string;
-    }
+  interface User {
+    role: string;
+  }
 }
 
 declare module "next-auth/jwt" {
-    interface JWT {
-        sub: string;
-        role: string;
-    }
+  interface JWT {
+    role: string;
+  }
 }
